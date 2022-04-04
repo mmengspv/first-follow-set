a4994f94a75a4950319910ee4ec02159af4055c8
mmengspv <supavet.a@ku.th>
2022-04-04 17:49:51 +0700

    add find follow set

--- first_set.py
+++ first_set.py
@@ -4,6 +4,8 @@
 
 import re
 
+regex = re.compile('[@_!#$%^&*()<>?/\|}{~:Є]')
+
 
 def first(pd):
     count = 0
@@ -27,10 +29,41 @@ def first(pd):
     return count
 
 
+def follow(pd):
+    count = 0
+    pd_split = pd.split("=")
+    nonterminal = pd_split[0]
+    data = pd_split[1]
+    for i in range(len(data)):
+        if data[i].isupper() and regex.search(data[i]) == None:
+            if i == len(data)-1:
+                for j in follow_set[nonterminal]:
+                    if j not in follow_set[data[i]]:
+                        follow_set[data[i]].append(j)
+                        count += 1
+            elif data[i+1].isupper() and regex.search(data[i+1]) == None:
+                for j in first_set[data[i+1]]:
+                    if j not in follow_set[data[i]] and j != "Є":
+                        follow_set[data[i]].append(j)
+                        count += 1
+                if "Є" in first_set[data[i+1]]:
+                    for j in follow_set[data[i+1]]:
+                        if j not in follow_set[data[i]]:
+                            follow_set[data[i]].append(j)
+                            count += 1
+            elif data[i+1].islower() or regex.search(data[i+1]) != None:
+                if data[i+1] not in follow_set[data[i]]:
+                    follow_set[data[i]].append(data[i+1])
+                    count += 1
+
+    return count
+
+
 num_production = int(input("Enter number of production: "))
 
 production = []
 first_set = {}
+follow_set = {}
 
 for i in range(num_production):
     production_input = input("Enter production :")
@@ -40,7 +73,6 @@ for i in range(num_production):
     if non_terminal not in first_set:
         first_set[non_terminal] = []
     for i in production_input[1]:
-        regex = re.compile('[@_!#$%^&*()<>?/\|}{~:Є]')
         if (i.islower() or regex.search(i) != None) and i not in first_set:
             first_set[i] = [i]
 
@@ -53,9 +85,37 @@ while(True):
     for i in production:
         count += first(i)
 
+
+for index, key in enumerate(production):
+    non_terminal = key.split("=")[0]
+    if index == 0:
+        follow_set[non_terminal] = ["$"]
+    else:
+        if non_terminal not in follow_set:
+            follow_set[non_terminal] = []
+
+
+count = 1
+while(True):
+    if count == 0:
+        break
+    count = 0
+    print("doloop")
+    for i in production:
+        count += follow(i)
+    print(count)
+    # break
+
 print()
 print("----------------------- First Set -------------------------")
 print()
 
 for x, y in first_set.items():
+    print("%s = %s" % (x, y))
+
+print()
+print("----------------------- Follow Set -------------------------")
+print()
+
+for x, y in follow_set.items():
     print("%s = %s" % (x, y))
